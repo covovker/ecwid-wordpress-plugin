a8cef9534a1f80fb664ca4852310866ea8cdd886
marvin <marvin@ecwid.com>
2014-08-20 18:10:27 +0400

    Minor fix for store page auto detection

--- ecwid-shopping-cart/ecwid-shopping-cart.php
+++ ecwid-shopping-cart/ecwid-shopping-cart.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.ecwid.com?source=wporg
 Description: Ecwid is a free full-featured shopping cart. It can be easily integrated with any Wordpress blog and takes less than 5 minutes to set up.
 Text Domain: ecwid-shopping-cart
 Author: Ecwid Team
-Version: 2.4.3.2
+Version: 2.4.3.1
 Author URI: http://www.ecwid.com?source=wporg
 */
 
@@ -619,10 +619,10 @@ function ecwid_content_started($content)
 
 function ecwid_wrap_shortcode_content($content, $name)
 {
-    return "<!-- Ecwid shopping cart plugin v 2.4.3.2 -->"
+    return "<!-- Ecwid shopping cart plugin v 2.4.3.1 -->"
 		   . ecwid_get_scriptjs_code()
 	       . "<div class=\"ecwid-shopping-cart-$name\">$content</div>"
-		   . "<!-- END Ecwid Shopping Cart v 2.4.3.2 -->";
+		   . "<!-- END Ecwid Shopping Cart v 2.4.3.1 -->";
 }
 
 function ecwid_get_scriptjs_code($force_lang = null) {
@@ -1211,7 +1211,7 @@ function ecwid_save_post($post_id)
 	if ($post_id == get_option('ecwid_store_page_id') || $post_id == get_option('ecwid_store_page_id_auto')) {
 		$new_status = get_post_status($post_id);
 
-		if (!in_array($new_status, array('publish', 'private'))) {
+		if (!in_array($new_status, array('publish', 'private')) || !ecwid_page_has_productbrowser($post_id)) {
 			$pages = get_pages(array('post_status' => 'publish,private'));
 
 			foreach ($pages as $page) {
