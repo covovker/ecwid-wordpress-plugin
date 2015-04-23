3ea2d727694e768c88bd068123b8541e5183e244
marvin <marvin@ecwid.com>
2015-04-23 17:52:09 +0300

    APPS-292

---
+++ ecwid-shopping-cart/templates/wp-dashboard-widget.php
@@ -0,0 +1,143 @@
+<table style="width: 100%; border: 0px solid #C0C0C0; background-color: #FFFFFF" cellSpacing=0 cellPadding=3>
+	<tr><td height=80 style="background-color: #FFFFFF">
+			<!-- Begin Ad Code -->
+			<script data-cfasync="false" type="text/javascript">
+				function loadTextAd(sparkCounter, id, setID) {
+					output179716="http://ab166273.adbutler-alion.com/adserve/;ID=" + id + ";setID=" + setID + ";type=textad;pid=" + rnd + ";layoutID=" + sparkCounter;
+					document.writeln("<SCR" + "IPT SRC=" + output179716 + " type=\"text/javascript\"> <\/scr" + "ipt>");
+				}
+				var loadedTextAds179716;
+				if(loadedTextAds179716 == null)
+					loadedTextAds179716= new Array();
+				var d = new Date();
+				var id179716 = 166273;
+				var setID179716 = 179716;
+				var rnd = window.rnd || Math.floor(Math.random()*10e6);
+				var sparkCounter179716;
+				if(sparkCounter179716 == null)
+					sparkCounter179716 = 1;
+				else
+					sparkCounter179716 = sparkCounter179716 + 1;
+				id="abta179716" + sparkCounter179716;
+				document.writeln("<div id='" + id + "'></div>");
+				loadedTextAds179716[sparkCounter179716] = false;
+				loadTextAd(sparkCounter179716, id179716, setID179716);
+			</script>
+
+			<!-- End Ad Code -->
+		</td></tr>
+	<tr><td height=80 style="background-color: #FFFFFF; border-top: 1px dashed #C0C0C0">
+			<!-- Begin Ad Code -->
+			<script data-cfasync="false" type="text/javascript">
+				function loadTextAd(sparkCounter, id, setID) {
+					output179716="http://ab166273.adbutler-alion.com/adserve/;ID=" + id + ";setID=" + setID + ";type=textad;pid=" + rnd + ";layoutID=" + sparkCounter;
+					document.writeln("<SCR" + "IPT SRC=" + output179716 + " type=\"text/javascript\"> <\/scr" + "ipt>");
+				}
+				var loadedTextAds179716;
+				if(loadedTextAds179716 == null)
+					loadedTextAds179716= new Array();
+				var d = new Date();
+				var id179716 = 166273;
+				var setID179716 = 179716;
+				var rnd = window.rnd || Math.floor(Math.random()*10e6);
+				var sparkCounter179716;
+				if(sparkCounter179716 == null)
+					sparkCounter179716 = 1;
+				else
+					sparkCounter179716 = sparkCounter179716 + 1;
+				id="abta179716" + sparkCounter179716;
+				document.writeln("<div id='" + id + "'></div>");
+				loadedTextAds179716[sparkCounter179716] = false;
+				loadTextAd(sparkCounter179716, id179716, setID179716);
+			</script>
+
+			<!-- End Ad Code -->
+		</td></tr>
+	<tr><td height=80 style="background-color: #FFFFFF; border-top: 1px dashed #C0C0C0">
+			<!-- Begin Ad Code -->
+			<script  data-cfasync="false" type="text/javascript">
+				function loadTextAd(sparkCounter, id, setID) {
+					output179716="http://ab166273.adbutler-alion.com/adserve/;ID=" + id + ";setID=" + setID + ";type=textad;pid=" + rnd + ";layoutID=" + sparkCounter;
+					document.writeln("<SCR" + "IPT SRC=" + output179716 + " type=\"text/javascript\"> <\/scr" + "ipt>");
+				}
+				var loadedTextAds179716;
+				if(loadedTextAds179716 == null)
+					loadedTextAds179716= new Array();
+				var d = new Date();
+				var id179716 = 166273;
+				var setID179716 = 179716;
+				var rnd = window.rnd || Math.floor(Math.random()*10e6);
+				var sparkCounter179716;
+				if(sparkCounter179716 == null)
+					sparkCounter179716 = 1;
+				else
+					sparkCounter179716 = sparkCounter179716 + 1;
+				id="abta179716" + sparkCounter179716;
+				document.writeln("<div id='" + id + "'></div>");
+				loadedTextAds179716[sparkCounter179716] = false;
+				loadTextAd(sparkCounter179716, id179716, setID179716);
+			</script>
+
+			<!-- End Ad Code -->
+		</td></tr>
+</table>
+<div class="ecwid-wp-dashboard-fallback">
+	<div class="item" style="border-bottom: 1px dashed #C0C0C0">
+		<div class="title">
+			<a target="_blank" href="?source=wporg-dashboard">Some link</a>
+		</div>
+		<div class="content">
+			Some content
+		</div>
+	</div>
+	<div class="item" style="border-bottom: 1px dashed #C0C0C0">
+		<div class="title">
+			<a target="_blank" href="?source=wporg-dashboard">Some link #2</a>
+		</div>
+		<div class="content">
+			Some content #3
+		</div>
+	</div>
+	<div class="item">
+		<div class="title">
+			<a target="_blank" href="?source=wporg-dashboard">Some link #3</a>
+		</div>
+		<div class="content">
+			Some content #3
+		</div>
+	</div>
+</div>
+<script data-cfasync="false" type="text/javascript">
+<!--
+(function() {
+	function is_ad_loaded() {
+		return jQuery('#abta1797161 table').length > 0;
+	}
+
+	function check_ads() {
+		if (is_ad_loaded()) {
+			debugger;
+			jQuery('#ecwid_dashboard_widget div.inside > table').show();
+			jQuery('#ecwid_dashboard_widget .ecwid-wp-dashboard-fallback').hide();
+		} else {
+			debugger;
+			jQuery('#ecwid_dashboard_widget div.inside > table').hide();
+			jQuery('#ecwid_dashboard_widget .ecwid-wp-dashboard-fallback').show();
+		}
+	}
+
+	var ecwid_wp_dashboard_interval = setInterval(function() {
+		check_ads();
+	}, 500);
+
+	check_ads();
+
+	setTimeout(function() {
+		if (!is_ad_loaded()) {
+			debugger;
+			clearInterval(ecwid_wp_dashboard_interval);
+		}
+	}, 3000);
+})();
+-->
+</script>
