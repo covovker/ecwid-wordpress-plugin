ed2420e0da72abe76998b20821749575e27aa7dd
marvin <marvin@ecwid.com>
2015-10-16 17:45:09 +0300

    APPS-482 fix & changes

--- ecwid-shopping-cart/includes/class-ecwid-oauth.php
+++ ecwid-shopping-cart/includes/class-ecwid-oauth.php
@@ -5,20 +5,10 @@ include ECWID_PLUGIN_DIR . "lib/phpseclib/AES.php";
 
 class Ecwid_OAuth {
 
-	const TOKEN_PLACEMENT_UNDEFINED = 0;
-	const TOKEN_PLACEMENT_DB = 1;
-	const TOKEN_PLACEMENT_DB_AND_FS = 2;
-
 	const OAUTH_CLIENT_ID = 'RD4o2KQimiGUrFZc';
 	const OAUTH_CLIENT_SECRET = 'jEPVdcA3KbzKVrG8FZDgNnsY3wKHDTF8';
 
-	const TEST_FILE_NAME = 'test_fs';
-	const TOKEN_FILE_NAME = 'token';
-
-	const TEST_OPTION_NAME = 'ecwid_test_vault';
-	const TOKEN_OPTION_NAME = 'ecwid_vault_token';
-
-	protected $_token_placement = 0;
+	const TOKEN_OPTION_NAME = 'ecwid_oauth_token';
 
     protected $crypt = null;
 
@@ -27,8 +17,6 @@ class Ecwid_OAuth {
 		add_action('admin_post_ecwid_oauth', array($this, 'process_authorization'));
 		add_action('admin_post_ecwid_disconnect', array($this, 'disconnect_store'));
 		add_action('admin_post_ecwid_show_reconnect', array($this, 'show_reconnect'));
-
-		$this->_load_params();
 
         $this->crypt = new Crypt_AES();
         $this->crypt->setIV(substr(md5(SECURE_AUTH_SALT . get_ecwid_store_id()), 0, 16));
@@ -85,6 +73,9 @@ class Ecwid_OAuth {
 		$query['redirect_uri']  = $params['redirect_uri'];
 		$query['response_type'] = 'code';
 		$query['scope']         = implode( ' ', $params['scopes'] );
+		foreach ($query as $key => $value) {
+			$query[$key] = urlencode($value);
+		}
 
 		return $url . '?' . build_query( $query );
 	}
@@ -119,7 +110,6 @@ class Ecwid_OAuth {
 		}
 
 		update_option( 'ecwid_store_id', $result->store_id );
-		$this->_prepare_storage();
 		$this->_save_token($result->access_token);
 
 		setcookie('ecwid_create_store_clicked', null, strtotime('-1 day'), ADMIN_COOKIE_PATH, COOKIE_DOMAIN);
@@ -189,19 +179,6 @@ class Ecwid_OAuth {
 		wp_redirect('admin.php?page=ecwid&connection_error=true');
 	}
 
-
-	protected function _prepare_storage()
-	{
-		$this->_token_placement = $this->_test_fs() ? self::TOKEN_PLACEMENT_DB_AND_FS : self::TOKEN_PLACEMENT_DB;
-
-		$everything_is_ok = $this->_test_vault();
-		if (!$everything_is_ok) {
-			$this->_token_placement = self::TOKEN_PLACEMENT_DB;
-		}
-
-		$this->_store_params();
-	}
-
 	public function get_oauth_token()
 	{
 		if ($this->is_initialized()) {
@@ -213,101 +190,32 @@ class Ecwid_OAuth {
 
 	public function is_initialized()
 	{
-		return in_array(
-			$this->_token_placement, array(self::TOKEN_PLACEMENT_DB, self::TOKEN_PLACEMENT_DB_AND_FS)
-		) && get_option(self::TOKEN_OPTION_NAME);
+		return get_option(self::TOKEN_OPTION_NAME);
 	}
 
-	protected function _store_params()
-	{
-		update_option('ecwid_oauth_placement', $this->_token_placement);
-	}
-
-	protected function _load_params()
-	{
-		$this->_token_placement = get_option('ecwid_oauth_placement');
-	}
-
-	protected function _test_fs()
-	{
-		if (!is_writable(ECWID_PLUGIN_DIR)) return false;
-
-		$testfile = ECWID_PLUGIN_DIR . '/' . self::TEST_FILE_NAME;
-		$message = 'this is a test message';
-
-		if (!file_put_contents($testfile, $message)) return false;
-
-		$result = file_get_contents($testfile);
-
-		return $result == $message;
-	}
-
-	protected function _test_vault()
-	{
-		$message = 'this is a test message';
-
-		$this->_save_token($message, true);
-
-		$loaded = $this->_load_token(true);
-
-        $this->_cleanup_test_data();
-
-		return $loaded == $message;
-	}
-
-	protected function _save_token($token, $test = false)
+	protected function _save_token($token)
 	{
 		$value = base64_encode($this->crypt->encrypt($token));
 
-		$filename = ECWID_PLUGIN_DIR . '/' . (!$test ? self::TOKEN_FILE_NAME : self::TEST_FILE_NAME);
-		$option_name = !$test ? self::TOKEN_OPTION_NAME : self::TEST_OPTION_NAME;
-
-		$fs_value = '';
-		$db_value = $value;
-
-		if ($this->_token_placement == self::TOKEN_PLACEMENT_DB_AND_FS) {
-			$fs_value = substr($value, 0, strlen($value) / 2);
-			$db_value = substr($value, strlen($value) / 2);
-			$result = file_put_contents($filename, $fs_value);
-		}
-
-		update_option($option_name, $db_value);
+		update_option(self::TOKEN_OPTION_NAME, $value);
 	}
 
-	protected function _load_token($test = false)
+	protected function _load_token()
 	{
-		$filename = ECWID_PLUGIN_DIR . '/' . (!$test ? self::TOKEN_FILE_NAME : self::TEST_FILE_NAME);
-		$option_name = !$test ? self::TOKEN_OPTION_NAME : self::TEST_OPTION_NAME;
 
-		$fs_value = '';
-		if ($this->_token_placement == self::TOKEN_PLACEMENT_DB_AND_FS) {
-			$fs_value = file_get_contents($filename);
-
-            if (empty($fs_value)) return false;
-		}
-
-		$db_value = get_option($option_name);
+		$db_value = get_option(self::TOKEN_OPTION_NAME);
         if (empty($db_value)) return false;
 
-		$encrypted = base64_decode($fs_value . $db_value);
-        if (empty($encrypted)) return false;
+		if (strlen($db_value == 64)) {
+			$encrypted = base64_decode($db_value);
+			if (empty($encrypted)) return false;
 
-		$token = $this->crypt->decrypt($encrypted);
-
-		return $token;
-	}
-
-	protected function _cleanup_test_data()
-	{
-		$filename = ECWID_PLUGIN_DIR . '/' . self::TEST_FILE_NAME;
-
-		$fs_is_available = $this->_test_fs();
-
-		if ($fs_is_available) {
-			unlink($filename);
+			$token = $this->crypt->decrypt($encrypted);
+		} else {
+			$token = $db_value;
 		}
 
-		delete_option(self::TEST_OPTION_NAME);
+		return $token;
 	}
 }
 
